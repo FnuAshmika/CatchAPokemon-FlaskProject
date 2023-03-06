dab565b889824e8acf2c2cabf8fe5766365a5bdd
Fnu Ashmika <ashmika.bv@gmail.com>
2023-03-06 15:56:20 -0600

    requirements.txt changed

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ import os
 class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Its Secret!!!'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
