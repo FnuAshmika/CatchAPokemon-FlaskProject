c42b312323a3adcce372fd75c33d6fd71f0bd7cf
Fnu Ashmika <ashmika.bv@gmail.com>
2023-03-06 15:12:26 -0600

    changes for hosting

--- app/__init__.py
+++ app/__init__.py
@@ -5,27 +5,33 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_cors import CORS
 
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
 
 
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+    app.config.from_object(Config)
 
-app = Flask(__name__)
-app.config.from_object(Config)
+    db.init_app(app)
+    migrate.init_app(app,db)
+    login.init_app(app)
+    
 
-db = SQLAlchemy(app)
-migrate = Migrate(app,db)
-login =LoginManager(app)
-cors = CORS(app)
+    login.login_view='/signin'
+    login.login_message="Don't forget to login."
 
-login.login_view='/signin'
-login.login_message="Don't forget to login."
+    from app.blueprints.auth import bp as auth_bp
+    app.register_blueprint(auth_bp)
 
-from app.blueprints.auth import bp as auth_bp
-app.register_blueprint(auth_bp)
+    from app.blueprints.main import bp as main_bp
+    app.register_blueprint(main_bp)
 
-from app.blueprints.main import bp as main_bp
-app.register_blueprint(main_bp)
+    from app.blueprints.social import bp as social_bp
+    app.register_blueprint(social_bp)
 
-from app.blueprints.social import bp as social_bp
-app.register_blueprint(social_bp)
+    return app
 
  
